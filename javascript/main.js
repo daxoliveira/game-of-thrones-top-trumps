6249eba78894c5d639e1249a4035173bb7e752cc
daxoliveira <dax-oliveira-br@gmail.com>
2019-06-23 12:33:51 -0300

    first functions completed

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,60 @@
-const deckStart = [
-    {Name:'Mustang', Picture:'mustangPic', Speed: 100, Acceleration: 110, HorsePower: 500},
-    {Name:'Camaro', Picture:'camaroPic', Speed: 200, Acceleration: 210, HorsePower: 600},
-    {Name:'Camry', Picture:'camryPic', Speed: 300, Acceleration: 310, HorsePower: 700},
-    {Name:'Civic', Picture:'civicPic', Speed: 120, Acceleration: 130, HorsePower: 300},
-    {Name:'Tacoma', Picture:'tacomaPic', Speed: 150, Acceleration: 160, HorsePower: 400},
-    {Name:'Corolla', Picture:'corollaPic', Speed: 160, Acceleration: 140, HorsePower: 350}
-];
+// Class constructor will be responsible for creating the main objects, game cards
 
-// function shuffleAndDistribute(arr){
-//     player1 = 0;
-//     player2 = 0;
-//     for(let i = 0; i < arr.length; i++){
+class Card {
+  constructor(name, picture, likeability, screenTime, numberOfKills) {
+    this.name = name;
+    this.picture = picture;
+    this.likeability = likeability;
+    this.screenTime = screenTime;
+    this.numberOfKills = numberOfKills;
+  }
+}
 
-//     }
-// } return 
+const daenerysTargaryen = new Card('Daenerys Targaryen', '1daenerysTargaryen', 100, 100, 100);
+const jonSnow = new Card('Jon Snow', '2jonSnow', 99, 99, 99);
+const tyrionLannister = new Card('Tyrion Lannister', '3tyrionLannister', 98, 98, 98);
+const sansaStark = new Card('Sansa Stark', '4sansaStark', 97, 97, 97);
+const cerseiLannister = new Card('Cersei Lannister', '5cerseiLannister', 96, 96, 96);
+const joffreyBaratheon = new Card('Joffrey Baratheon', '6joffreyBaratheon', 95, 95, 95);
+const eddardStark = new Card('Eddard Stark', '7eddardStark', 94, 94, 94);
+const aryaStark = new Card('Arya Stark', '8aryaStark', 93, 93, 93);
+const brienneOfTarth = new Card('Brienne of Tarth', '9brienneOfTarth', 92, 92, 92);
+const ramsayBolton = new Card('Ramsay Bolton', '10ramsayBolton', 91, 91, 91);
 
-console.log(shuffle(deckStart));
+const sharedDeck = [];
 
+sharedDeck.push(daenerysTargaryen, jonSnow, tyrionLannister, sansaStark, cerseiLannister, joffreyBaratheon, eddardStark, aryaStark, brienneOfTarth, ramsayBolton);
 
+class Game {
+  constructor() {
+    this.shuffledCards = this.pickCardsRandomly(sharedDeck);
+    this.player = this.shuffledCards[0];
+    this.computer = this.shuffledCards[1];
+  }
+
+  pickCardsRandomly(deck) {
+    let cardsPlayer = [];
+    let cardsComputer = [];
+    
+    let count = 0;
+    while (count < 10) {
+      let randomCard = Math.floor(Math.random() * deck.length)
+      if (cardsPlayer.length > cardsComputer.length)
+      cardsComputer.push(deck.splice(randomCard, 1));
+      else
+      cardsPlayer.push(deck.splice(randomCard, 1));
+      count += 1;
+    }
+    let cardsArrs = [];
+    cardsArrs.push(cardsPlayer, cardsComputer);
+    return cardsArrs;
+  }
+}
+
+let gameStart = new Game();
+
+console.log(gameStart.player[0]);
+
+let gameRound = () => {
+  
+};

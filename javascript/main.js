dafdef3cbc7a66dc4be7a00d3eccca2eb45c5947
daxoliveira <dax-oliveira-br@gmail.com>
2019-06-24 14:12:20 -0300

    game start logic iteration

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-// Class constructor will be responsible for creating the main objects, game cards
+// Step 1 - Set up class constructor of card objects
 
 class Card {
   constructor(name, picture, likeability, screenTime, numberOfKills) {
@@ -10,51 +10,67 @@ class Card {
   }
 }
 
-const daenerysTargaryen = new Card('Daenerys Targaryen', '1daenerysTargaryen', 100, 100, 100);
-const jonSnow = new Card('Jon Snow', '2jonSnow', 99, 99, 99);
-const tyrionLannister = new Card('Tyrion Lannister', '3tyrionLannister', 98, 98, 98);
-const sansaStark = new Card('Sansa Stark', '4sansaStark', 97, 97, 97);
-const cerseiLannister = new Card('Cersei Lannister', '5cerseiLannister', 96, 96, 96);
-const joffreyBaratheon = new Card('Joffrey Baratheon', '6joffreyBaratheon', 95, 95, 95);
-const eddardStark = new Card('Eddard Stark', '7eddardStark', 94, 94, 94);
-const aryaStark = new Card('Arya Stark', '8aryaStark', 93, 93, 93);
-const brienneOfTarth = new Card('Brienne of Tarth', '9brienneOfTarth', 92, 92, 92);
-const ramsayBolton = new Card('Ramsay Bolton', '10ramsayBolton', 91, 91, 91);
+// Step 2 - Create 10 cards and store them in an array
 
-const sharedDeck = [];
+const daenerysTargaryen = new Card('Daenerys Targaryen', './images/1daenerysTargaryen.jpg', 100, 100, 100);
+const jonSnow = new Card('Jon Snow', './images/2jonSnow.jpg', 99, 99, 99);
+const tyrionLannister = new Card('Tyrion Lannister', './images/3tyrionLannister.jpg', 98, 98, 98);
+const sansaStark = new Card('Sansa Stark', './images/4sansaStark.jpg', 97, 97, 97);
+const cerseiLannister = new Card('Cersei Lannister', './images/5cerseiLannister.jpg', 96, 96, 96);
+const joffreyBaratheon = new Card('Joffrey Baratheon', './images/6joffreyBaratheon.jpg', 95, 95, 95);
+const eddardStark = new Card('Eddard Stark', './images/7eddardStark.jpg', 94, 94, 94);
+const aryaStark = new Card('Arya Stark', './images/8aryaStark.jpg', 93, 93, 93);
+const brienneOfTarth = new Card('Brienne of Tarth', './images/9brienneOfTarth.jpg', 92, 92, 92);
+const ramsayBolton = new Card('Ramsay Bolton', './images/10ramsayBolton.jpg', 91, 91, 91);
 
-sharedDeck.push(daenerysTargaryen, jonSnow, tyrionLannister, sansaStark, cerseiLannister, joffreyBaratheon, eddardStark, aryaStark, brienneOfTarth, ramsayBolton);
+const unshuffledDeck = [];
+unshuffledDeck.push(daenerysTargaryen, jonSnow, tyrionLannister, sansaStark, cerseiLannister, joffreyBaratheon, eddardStark, aryaStark, brienneOfTarth, ramsayBolton);
 
-class Game {
-  constructor() {
-    this.shuffledCards = this.pickCardsRandomly(sharedDeck);
-    this.player = this.shuffledCards[0];
-    this.computer = this.shuffledCards[1];
-  }
+// Step 3 - "Game Start Button" - Set shuffle,deal, and display functions ------------------------
+// a) Shuffle - Shuffle the 10 cards
 
-  pickCardsRandomly(deck) {
-    let cardsPlayer = [];
-    let cardsComputer = [];
-    
-    let count = 0;
-    while (count < 10) {
-      let randomCard = Math.floor(Math.random() * deck.length)
-      if (cardsPlayer.length > cardsComputer.length)
-      cardsComputer.push(deck.splice(randomCard, 1));
-      else
-      cardsPlayer.push(deck.splice(randomCard, 1));
-      count += 1;
-    }
-    let cardsArrs = [];
-    cardsArrs.push(cardsPlayer, cardsComputer);
-    return cardsArrs;
+const shuffledDeck = [];
+
+const shuffle = () => {
+  for (let i = 0; i < 10; i += 1) {
+    const random = Math.floor(Math.random() * unshuffledDeck.length);
+    shuffledDeck.push(unshuffledDeck[random]);
+    unshuffledDeck.splice(random, 1);
   }
 }
 
-let gameStart = new Game();
 
-console.log(gameStart.player[0]);
+// b) Deal - Divide into 2 arrays and assign the first array to Player and the second to Computer
 
-let gameRound = () => {
-  
+let playerDeck;
+let computerDeck;
+
+const deal = () => {
+  playerDeck = shuffledDeck.slice(0, shuffledDeck.length / 2);
+  computerDeck = shuffledDeck.slice(5);
 };
+
+// Place - placement of each array’s first card
+
+let playerCard;
+let computerCard;
+
+const place = () => {
+  playerCard = playerDeck[0];
+  computerCard = computerDeck[0];
+}
+
+const startGame = () => {
+  shuffle();
+  deal();
+  place();
+};
+
+startGame();
+
+// console.log('---------------------PLAYER-----------------------------');
+// console.log(playerCard);
+// console.log('---------------------COMPUTER--------------------------');
+// console.log(computerCard);
+
+// Step 4
